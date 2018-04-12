12a7320b4d939d8f9c40c86011d5770d84bd1a06
Natalie Ryder <nryder121@pointloma.edu>
2018-04-11 21:55:24 -0700

    added scrape route back in

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,9 @@
 const router = require("express").Router();
 const classRoutes = require("./classes");
+const scrape = require("../../scripts/scrape.js");
 
 // Book routes
 router.use("/classes", classRoutes);
+router.get("/scrape", scrape);
 
 module.exports = router;
