a233ca6c44d8b982327cb033bec461b8c4863505
Natalie Ryder <nryder121@pointloma.edu>
2018-04-16 14:19:41 -0700

    updated lesson page for real data

--- client/src/components/Lesson/LessonPage.js
+++ client/src/components/Lesson/LessonPage.js
@@ -20,7 +20,7 @@ export default class LessonPage extends Component {
     getLesson = id => {
         API.getLessonById(id)
         .then(response => {
-            this.setState({lesson: response.data});
+            this.setState({lesson: response.data}, () => console.log(this.state.lesson));
         })
         .catch(err => console.log(err));
     }
@@ -30,16 +30,27 @@ export default class LessonPage extends Component {
             <div style={{ padding: 10 }}>
                 <div className="ClassCard">
                     <h2> {this.state.lesson.title}</h2>
-                    <div>Category {this.state.lesson.category}</div>
-                    <div> {this.state.lesson.description}</div>
+                    <div>Category: {this.state.lesson.category}</div>
+                    <div dangerouslySetInnerHTML={{__html: this.state.lesson.description}}></div>
                     <img src={this.state.lesson.image_url} />
-                    <div>Start Date:{this.state.lesson.startdate ? this.state.lesson.startdate : this.state.lesson.classTimes.join(',')}</div>
+                    <div>Start Date: {this.state.lesson.startDate} </div>
+
+                    {this.state.lesson.classTimes 
+                        ? 
+                        (<div> Class Times:
+                            <ul>
+                            {this.state.lesson.classTimes.map(each => (<li>{each}</li>))}
+                            </ul>
+                        </div>)
+                            : <div>Start Time: {this.state.lesson.startTime}</div>
+                         
+                        }
+                   
                      {/* if statment in here to show if starttime.length >1 then show that otherwise  */}
-                    <div>Start Time: {this.state.lesson.starttime} </div>
                     <div>Register Link:{this.state.lesson.registerLink}</div>
                     <div>Location: {this.state.lesson.location}</div>
                     <div>Register Options:{this.state.lesson.registrationOptions}</div>
-                    <button> Register {() => { this.openLessonPage() }}</button>
+                    <a class="btn btn-primary" target="_blank" href={this.state.lesson.registerLink}>Register</a>
                     
                 </div>
             </div>
