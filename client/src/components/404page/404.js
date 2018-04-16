ecb91c764d8e602f23f1613f32f90ac6de5da2fa
Sheila Nasehi (BDA/Outsourcing/Shared Svcs <sheila.nasehi@towerswatson.com>
2018-04-15 19:31:10 -0700

    404 js file

--- client/src/components/404page/404.js
+++ client/src/components/404page/404.js
@@ -1,1 +1,12 @@
 //match function for error page 
+
+// components/NotFound.js
+import React from 'react';
+
+const NotFound = () =>
+    <div>
+        <h3>404 page not found</h3>
+        <p>We are sorry but the page you are looking for does not exist.</p>
+    </div>
+
+export default NotFound;
