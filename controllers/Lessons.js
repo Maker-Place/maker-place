f38a119e6d148b947fb50eeeabc233aa0ee4285f
krose1313 <kathrynrose40@gmail.com>
2018-04-16 21:23:10 -0700

    rendering membership page data

--- controllers/Lessons.js
+++ controllers/Lessons.js
@@ -1,6 +1,6 @@
 const db = require("../models");
 
-// Defining methods for the booksController
+// Defining methods for the classes controllers
 module.exports = {
   findAll: function(req, res) {
     res.json("findAll");
