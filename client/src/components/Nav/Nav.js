13d62bb7a0095735f6e3ab6c2bbc20027f4a520f
Natalie Ryder <natalie@natalieryder.com>
2018-04-16 19:03:26 -0700

    Add category navigation to header
    - change App.js to a stateful component and get categories
    - add dropdown to classes with the categories
    - LessonsPage.js gets categories as props
    -

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -7,10 +7,10 @@ import {
 import './Nav.css';
 
 
-const Nav = () => (
+const Nav = props => (
   <nav className="navbar navbar-expand-md navbar-dark bg-dark">
 
-     <a className="navbar-brand" href="#"><img src={process.env.PUBLIC_URL + '/assets/images/MakerPlace_VersionB.svg'} alt="MakerPlace"/></a>
+     <a className="navbar-brand" href="/"><img src={process.env.PUBLIC_URL + '/assets/images/MakerPlace_VersionB.svg'} alt="MakerPlace"/></a>
 
      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
        <span className="navbar-toggler-icon"></span>
@@ -24,12 +24,22 @@ const Nav = () => (
          <li className="nav-item">
            <Link className="nav-link" to="/About">About Us</Link>
          </li>
-         <li className="nav-item">
-           <Link className="nav-link" to="/lessons">Classes</Link>
-         </li>
+         <li className="nav-item dropdown">
+        <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Classes
+        </a>
+        <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+          {props.categories.map(category => (
+              
+                <Link className="dropdown-link"key={category} to={"/lessons/" + category}>{category}</Link>
+              
+            ))}
+        </div>
+      </li>
          <li className="nav-item">
            <Link className="nav-link" to="/memberships">Memberships</Link>
          </li>
+
        </ul>
        <form className="form-inline my-2 my-lg-0">
          <button className="btn btn-secondary my-2 my-sm-0" type="submit">Login</button>
