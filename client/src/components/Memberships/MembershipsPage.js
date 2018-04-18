311b6e7fc2b93bc9040879e20b3a3078f674de01
Jeff Ray <rayinsf@gmail.com>
2018-04-17 20:16:53 -0700

    here is all of the membership stylings

--- client/src/components/Memberships/MembershipsPage.js
+++ client/src/components/Memberships/MembershipsPage.js
@@ -30,27 +30,34 @@ class Memberships extends Component {
       <div className="membership-page">
         <div className="membership-image">
           <div className="membership-text">
-          
-            <br></br>
-            <p>Corporate and family memberships are available in person at MakerPlace.
+            <div className="membership-header">
+              <div className="membership-title">
+                Memberships
+              </div>
+              <div className="membership-description">
+                Corporate and family memberships are available in person at MakerPlace.
          Military, student, and senior citizen (55+) discounts available with a valid ID for 
          the "Month to Month" membership level only.
-            </p>
-            {memberships.map(membership => {
-              return (
-                <div className="card" style={{width: "18rem"}}>
-                  <div className="card-body">
-                    <h1 className="card-title">{membership.membership_type}</h1>
-                    <h2 className="card-annual-cost">{membership.annual_cost}</h2>
-                    <h3 className="card-monthly-cost">{membership.monthly_cost}</h3>
-                    <p className="card-discount">{membership.discount}</p>
-                    <p className="card-text">{membership.description}</p>
-                    <a href="#" className="btn btn-primary">Start Making...</a>
+              </div>
+            </div>
+            {/* add wrapper for grouping cards together */}
+            <div className="cards">
+              {memberships.map(membership => {
+                return (
+                  <div className="card">
+                    <div className="card-body">
+                      <h1 className="card-title">{membership.membership_type}</h1>
+                      <h2 className="card-annual-cost">{membership.annual_cost}</h2>
+                      <h3 className="card-monthly-cost">{membership.monthly_cost}</h3>
+                      <p className="card-discount">{membership.discount}</p>
+                      <p className="card-text">{membership.description}</p>
+                      <a href="#" className="btn btn-primary">Start Making...</a>
+                    </div>
                   </div>
-                </div>
-                )
-              })
-            }
+                  )
+                })
+              }
+            </div>
           </div>
         </div>
       </div>
