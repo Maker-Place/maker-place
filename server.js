f0fa4a610df85b88c513c8e425095e7aae411f6e
Natalie Ryder <natalie@natalieryder.com>
2018-04-21 14:04:54 -0700

    test heroku

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 // Serve up static assets
 //we need to use the next line to push the app in heroku
-// app.use(express.static("client/build"));
+app.use(express.static("client/build"));
 // Add routes, both API and view
 app.use(routes);
 // Set up promises with mongoose
