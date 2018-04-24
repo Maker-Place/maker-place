2eb290382d8f61621b48fac5766db3744799b73d
Natalie Ryder <natalie@natalieryder.com>
2018-04-24 14:01:31 -0700

    logging for debugging purposes

--- libs/auth.js
+++ libs/auth.js
@@ -1,15 +1,23 @@
 module.exports = {
     ensureAuthenticated: function (req, res, next) {
-      if (req.isAuthenticated())
-        return next();
+      console.log("ensure isAuthenticated");
+      if (req.isAuthenticated()) {
+        console.log("you're authenticated");
+        return next()
+      } else {
+        console.log("you're not logged in");
+      }
+
       res.redirect('/login');
     },
     ensureGuest: function (req, res, next) {
       console.log("ensureGuest");
-      if (req.isAuthenticated())
+      if (req.isAuthenticated()) {
+        console.log("you're logged in");
         res.redirect('/secret');
-      else
+      } else {
         return next();
+      }
     }
   }
   
